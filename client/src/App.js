3ef6801254248a39593535108270e3d156922b2c
thenm <nitinmaul.nitty@gmail.com>
2019-09-17 08:45:45 +0530

    Redux and Backend connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import  AppNavBar from './components/AppNavbar'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React, { Component } from 'react';
+import AppNavbar from './components/AppNavbar';
+import BookList from './components/BookList';
+import ItemModal from './components/ItemModal'
+import { Container } from 'reactstrap';
+
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <AppNavBar></AppNavBar>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <Container>
+            <ItemModal />
+            <BookList />
+          </Container>
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
