926b0dc69d345c0f42547239a5c08423f94146d6
thenm <nitinmaul.nitty@gmail.com>
2019-09-19 08:40:35 +0530

    Typo fixed

--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -56,27 +56,27 @@ class ItemModal extends Component {
 						style={{ marginBottom: '2rem' }}
 						onClick={this.toggle}
 					>
-						Add Item
+						Add Book
 					</Button>
 				) : (
 					<h4 className="mb-3 ml-4">Please log in to manage books</h4>
 				)}
 
 				<Modal isOpen={this.state.modal} toggle={this.toggle}>
-					<ModalHeader toggle={this.toggle}>Add To Shopping List</ModalHeader>
+					<ModalHeader toggle={this.toggle}>Add book to Reading List</ModalHeader>
 					<ModalBody>
 						<Form onSubmit={this.onSubmit}>
 							<FormGroup>
-								<Label for="item">Item</Label>
+								<Label for="item">Book</Label>
 								<Input
 									type="text"
 									name="name"
 									id="item"
-									placeholder="Add shopping item"
+									placeholder="Add book to read"
 									onChange={this.onChange}
 								/>
 								<Button color="dark" style={{ marginTop: '2rem' }} block>
-									Add Item
+									Add Book
 								</Button>
 							</FormGroup>
 						</Form>
